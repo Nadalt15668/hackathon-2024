5f245f555e0b6c640a23043449c784ff9efa63e3
miunim 4 <miunim4@bsmch.net>
2024-10-13 18:13:54 +0300

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./router";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <Typography />
     </>
   );
 }
